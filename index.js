473acf3b6be3ce96e08afcd4ff91379846252c29
ajsfkx <ajs.fkx@gmail.com>
2022-11-10 20:35:53 -0400

    Added C/F temp conersion

--- index.js
+++ index.js
@@ -44,9 +44,47 @@ function apiCall(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  celciusTemperature = response.data.main.temp;
 }
 
-let apikey = "73a00877081bd43422bdee0f3022beb5";
-let city = "Chandausi";
-let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
-axios.get(apiurl).then(apiCall);
+function search(city) {
+  let apikey = "73a00877081bd43422bdee0f3022beb5";
+  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+  axios.get(apiurl).then(apiCall);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+let celciusTemperature = null;
+
+function farenheitTemperature(event) {
+  event.preventDefault();
+  celciusTemp.classList.remove("abc");
+  farenheitTemp.classList.add("abc");
+  let temperatureElement = document.querySelector("#temp");
+  let finalFarenheit = (celciusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(finalFarenheit);
+}
+
+function celciusTemperaturee(event) {
+  event.preventDefault();
+  celciusTemp.classList.add("abc");
+  farenheitTemp.classList.remove("abc");
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let farenheitTemp = document.querySelector("#temp-farenheit");
+farenheitTemp.addEventListener("click", farenheitTemperature);
+
+let celciusTemp = document.querySelector("#temp-celcius");
+celciusTemp.addEventListener("click", celciusTemperaturee);
+
+search("New York");
